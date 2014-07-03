f9dffa72086f93fd8ffaa0b42857eb98566d727c
Nitin Mohan <nitin@rightscale.com>
2014-07-03 13:47:15 -0700

    Remove unneccesary context blocks

--- spec/praxis/response_definition_spec.rb
+++ spec/praxis/response_definition_spec.rb
@@ -17,71 +17,69 @@ describe Praxis::ResponseDefinition do
   its(:status) { should == 200 }
   its(:description) { should == 'test description' }
 
-  context 'for fields that can take multiple types' do
-    context 'such as media_type' do
-      it 'accepts a MediaType object and returns the media_type that was set' do
-        response_definition.media_type ExampleMediaType
-        expect(response_definition.media_type).to be(ExampleMediaType)
-      end
-
-      it 'accepts a string and returns a SimpleMediaType' do
-        response_definition.media_type 'string'
-        expect(response_definition.media_type).to be_kind_of(Praxis::SimpleMediaType)
-      end
-
-      it 'accepts a Symbol :controller_defined' do
-        response_definition.media_type :controller_defined
-        expect(response_definition.media_type).to be(:controller_defined)
-      end
-
-      it 'should return an error when media_type is not a String or a MediaType' do
-        expect{ response_definition.media_type Object.new }.to raise_error(/Invalid media_type specification/)
-      end
-
-      it 'should return an error when media_type is a Symbol other than :controller_defined' do
-        expect{ response_definition.media_type :symbol }.to raise_error(/Invalid media_type specification/)
-      end
+  context '#media_type' do
+    it 'accepts a MediaType object and returns the media_type that was set' do
+      response_definition.media_type ExampleMediaType
+      expect(response_definition.media_type).to be(ExampleMediaType)
     end
 
-    context 'such as location' do
-      it 'accepts a String' do
-        response_definition.location "string_location"
-        expect(response_definition.location).to eq("string_location")
-      end
-
-      it 'accepts a Regex' do
-        response_definition.location /regex_location/
-        expect(response_definition.location).to eq(/regex_location/)
-      end
-
-      it 'should return an error when location is not a Regex or a String object' do
-        expect { response_definition.location Object.new }.to raise_error(/Invalid location specification/)
-      end
+    it 'accepts a string and returns a SimpleMediaType' do
+      response_definition.media_type 'string'
+      expect(response_definition.media_type).to be_kind_of(Praxis::SimpleMediaType)
     end
 
-    context 'such as headers' do
-      it 'accepts a Hash' do
-        response_definition.headers Hash["X-Header" => "value", "Content-Type" => "some_type"]
-        expect(response_definition.headers).to be_a(Hash)
-      end
+    it 'accepts a Symbol :controller_defined' do
+      response_definition.media_type :controller_defined
+      expect(response_definition.media_type).to be(:controller_defined)
+    end
 
-      it 'accepts an Array' do
-        response_definition.headers ["X-Header: value", "Content-Type: some_type"]
-        expect(response_definition.headers).to eq(["X-Header: value", "Content-Type: some_type"])
-      end
+    it 'should return an error when media_type is not a String or a MediaType' do
+      expect{ response_definition.media_type Object.new }.to raise_error(/Invalid media_type specification/)
+    end
 
-      it 'accepts a String' do
-        response_definition.headers "X-Header: value"
-        expect(response_definition.headers).to eq("X-Header: value")
-      end
-
-      it 'should return an error when headers are not a Hash, Array or String object' do
-        expect{ response_definition.headers Object.new }. to raise_error(/Invalid headers specification/)
-      end
+    it 'should return an error when media_type is a Symbol other than :controller_defined' do
+      expect{ response_definition.media_type :symbol }.to raise_error(/Invalid media_type specification/)
     end
   end
 
-  context 'with multipart' do
+  context '#location' do
+    it 'accepts a String' do
+      response_definition.location "string_location"
+      expect(response_definition.location).to eq("string_location")
+    end
+
+    it 'accepts a Regex' do
+      response_definition.location /regex_location/
+      expect(response_definition.location).to eq(/regex_location/)
+    end
+
+    it 'should return an error when location is not a Regex or a String object' do
+      expect { response_definition.location Object.new }.to raise_error(/Invalid location specification/)
+    end
+  end
+
+  context '#headers' do
+    it 'accepts a Hash' do
+      response_definition.headers Hash["X-Header" => "value", "Content-Type" => "some_type"]
+      expect(response_definition.headers).to be_a(Hash)
+    end
+
+    it 'accepts an Array' do
+      response_definition.headers ["X-Header: value", "Content-Type: some_type"]
+      expect(response_definition.headers).to eq(["X-Header: value", "Content-Type: some_type"])
+    end
+
+    it 'accepts a String' do
+      response_definition.headers "X-Header: value"
+      expect(response_definition.headers).to eq("X-Header: value")
+    end
+
+    it 'should return an error when headers are not a Hash, Array or String object' do
+      expect{ response_definition.headers Object.new }. to raise_error(/Invalid headers specification/)
+    end
+  end
+
+  context '#multipart' do
     subject(:multipart) { response.multipart }
 
     context 'using default envelope status' do

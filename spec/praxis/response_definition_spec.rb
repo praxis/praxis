655546bd49597dbe551d652650ac3da92e1b3fe2
Dane Jensen <dane@rightscale.com>
2014-08-04 17:30:13 -0700

    Add specs for ResponseDefininition#validate_parts!

--- spec/praxis/response_definition_spec.rb
+++ spec/praxis/response_definition_spec.rb
@@ -17,6 +17,7 @@ describe Praxis::ResponseDefinition do
   its(:parts) { should be(nil) }
   let(:response_status) { 200 }
   let(:response_headers) { { "X-Header" => "value", "Content-Type" => "some_type"} }
+
   let(:response) { instance_double("Praxis::Response", status: response_status , headers: response_headers ) }
 
 
@@ -116,11 +117,11 @@ describe Praxis::ResponseDefinition do
 
     context 'with a proc' do
       let(:the_proc) do
-         Proc.new do
-           status 201
-           media_type 'from_proc'
-         end
-       end
+        Proc.new do
+          status 201
+          media_type 'from_proc'
+        end
+      end
 
       before do
         response_definition.parts the_proc
@@ -137,9 +138,9 @@ describe Praxis::ResponseDefinition do
     context 'with a block' do
       before do
         response_definition.parts do
-           status 201
-           media_type 'from_proc'
-         end
+          status 201
+          media_type 'from_proc'
+        end
       end
 
       subject(:parts) { response_definition.parts }
@@ -150,48 +151,48 @@ describe Praxis::ResponseDefinition do
 
     end
   end
-#  context '#multipart' do
-#    subject(:multipart) { response.multipart }
-#
-#    context 'using default envelope status' do
-#      let(:response) do
-#        Praxis::ResponseDefinition.new(name) do
-#          status 500
-#          multipart :always
-#        end
-#      end
-#      it { should_not be_nil }
-#      its(:name) { should be(:always) }
-#      its(:status) { should be(200) }
-#    end
-#
-#    context 'defining the envelope' do
-#      let(:response) do
-#        Praxis::ResponseDefinition.new(name) do
-#          status 500
-#          multipart :optional do
-#            status 202
-#            headers 'Some-Header'
-#          end
-#        end
-#      end
-#      its(:name) { should be(:optional) }
-#      its(:status) { should be(202) }
-#      its(:headers) { should eq('Some-Header') }
-#    end
-#
-#    context 'an invalid multipart mode' do
-#      it 'raises an error' do
-#        expect {
-#          Praxis::ResponseDefinition.new(name) do
-#            status 200
-#            multipart :never
-#          end
-#        }.to raise_error(/Invalid multipart mode/)
-#      end
-#    end
-#  end
-#
+  #  context '#multipart' do
+  #    subject(:multipart) { response.multipart }
+  #
+  #    context 'using default envelope status' do
+  #      let(:response) do
+  #        Praxis::ResponseDefinition.new(name) do
+  #          status 500
+  #          multipart :always
+  #        end
+  #      end
+  #      it { should_not be_nil }
+  #      its(:name) { should be(:always) }
+  #      its(:status) { should be(200) }
+  #    end
+  #
+  #    context 'defining the envelope' do
+  #      let(:response) do
+  #        Praxis::ResponseDefinition.new(name) do
+  #          status 500
+  #          multipart :optional do
+  #            status 202
+  #            headers 'Some-Header'
+  #          end
+  #        end
+  #      end
+  #      its(:name) { should be(:optional) }
+  #      its(:status) { should be(202) }
+  #      its(:headers) { should eq('Some-Header') }
+  #    end
+  #
+  #    context 'an invalid multipart mode' do
+  #      it 'raises an error' do
+  #        expect {
+  #          Praxis::ResponseDefinition.new(name) do
+  #            status 200
+  #            multipart :never
+  #          end
+  #        }.to raise_error(/Invalid multipart mode/)
+  #      end
+  #    end
+  #  end
+  #
 
   context '#validate' do
     context 'functional test' do
@@ -256,87 +257,87 @@ describe Praxis::ResponseDefinition do
         end
       end
 
-       describe "#validate_headers!" do
-         before { response_definition.headers(headers) }
-         context 'checking headers are set' do
-           context 'when there are missing headers' do
-             let (:headers) { { 'X-some' => 'test' } }
-             it 'should raise error' do
-               expect {
-                 response_definition.validate_headers!(response)
-               }.to raise_error(/headers missing/)
-             end
-           end
+      describe "#validate_headers!" do
+        before { response_definition.headers(headers) }
+        context 'checking headers are set' do
+          context 'when there are missing headers' do
+            let (:headers) { { 'X-some' => 'test' } }
+            it 'should raise error' do
+              expect {
+                response_definition.validate_headers!(response)
+              }.to raise_error(/headers missing/)
+            end
+          end
 
-           context "when headers specs are name strings" do
-             context "and is missing" do
-               let (:headers) { [ "X-Just-Key" ] }
-               it 'should raise error' do
-                 expect {
-                 response_definition.validate_headers!(response)
-                 }.to raise_error(/headers missing/)
-               end
-             end
+          context "when headers specs are name strings" do
+            context "and is missing" do
+              let (:headers) { [ "X-Just-Key" ] }
+              it 'should raise error' do
+                expect {
+                  response_definition.validate_headers!(response)
+                }.to raise_error(/headers missing/)
+              end
+            end
 
-             context "and is not missing" do
-               let (:headers) { [ "X-Header" ] }
-               it 'should not raise error' do
-                 expect {
-                 response_definition.validate_headers!(response)
-                 }.not_to raise_error
-               end
-             end
-           end
+            context "and is not missing" do
+              let (:headers) { [ "X-Header" ] }
+              it 'should not raise error' do
+                expect {
+                  response_definition.validate_headers!(response)
+                }.not_to raise_error
+              end
+            end
+          end
 
-           context "when header specs are hashes" do
-             context "and is missing" do
-               let (:headers) {
-                 [ { "X-Header" => "notfoodbar" } ]
-               }
-               it 'should raise error' do
-                 expect {
-                   response_definition.validate_headers!(response)
-                 }.to raise_error(/headers missing/)
-               end
-             end
+          context "when header specs are hashes" do
+            context "and is missing" do
+              let (:headers) {
+                [ { "X-Header" => "notfoodbar" } ]
+              }
+              it 'should raise error' do
+                expect {
+                  response_definition.validate_headers!(response)
+                }.to raise_error(/headers missing/)
+              end
+            end
 
-             context "and is not missing" do
-               let (:headers) {
-                 [ { "X-Header" => "value" } ]
-               }
-               it 'should not raise error' do
-                 expect {
-                   response_definition.validate_headers!(response)
-                 }.not_to raise_error
-               end
-             end
-           end
+            context "and is not missing" do
+              let (:headers) {
+                [ { "X-Header" => "value" } ]
+              }
+              it 'should not raise error' do
+                expect {
+                  response_definition.validate_headers!(response)
+                }.not_to raise_error
+              end
+            end
+          end
 
-           context "when header specs are of mixed type " do
-             context "and is missing" do
-               let (:headers) {
-                 [ { "X-Header" => "value" }, "not-gonna-find-me" ]
-               }
-               it 'should raise error' do
-                 expect {
-                   response_definition.validate_headers!(response)
-                 }.to raise_error(/headers missing/)
-               end
-             end
+          context "when header specs are of mixed type " do
+            context "and is missing" do
+              let (:headers) {
+                [ { "X-Header" => "value" }, "not-gonna-find-me" ]
+              }
+              it 'should raise error' do
+                expect {
+                  response_definition.validate_headers!(response)
+                }.to raise_error(/headers missing/)
+              end
+            end
 
-             context "and is not missing" do
-               let (:headers) {
-                 [ { "X-Header" => "value" }, "Content-Type" ]
-               }
-               it 'should not raise error' do
-                 expect {
-                   response_definition.validate_headers!(response)
-                 }.not_to raise_error
-               end
-             end
-           end
-         end
-       end
+            context "and is not missing" do
+              let (:headers) {
+                [ { "X-Header" => "value" }, "Content-Type" ]
+              }
+              it 'should not raise error' do
+                expect {
+                  response_definition.validate_headers!(response)
+                }.not_to raise_error
+              end
+            end
+          end
+        end
+      end
 
       describe "#validate_content_type!" do
 
@@ -358,129 +359,70 @@ describe Praxis::ResponseDefinition do
         end
 
         context 'for definition that includes media_type defined' do
-            before { response_definition.media_type(media_type) }
+          before { response_definition.media_type(media_type) }
 
-            context 'when content type matches the mediatype of the spec' do
-              let(:response_headers) { {'Content-Type' => content_type } }
-              it 'should raise error telling you so' do
-                expect {
-                  response_definition.validate_content_type!(response)
-                }.to_not raise_error
-              end
-            end
-
-            context 'when content type does not match' do
-              let(:response_headers) { {'Content-Type' => "will_never_match" } }
-              it 'should raise error telling you so' do
-                expect {
-                  response_definition.validate_content_type!(response)
-                }.to raise_error(/Bad Content-Type/)
-              end
-            end
-
-            context 'when content type is not set' do
-              let(:response_headers) { {} }
-              it 'should still raise an error' do
-                expect {
-                  response_definition.validate_content_type!(response)
-                }.to raise_error(/Bad Content-Type/)
-              end
+          context 'when content type matches the mediatype of the spec' do
+            let(:response_headers) { {'Content-Type' => content_type } }
+            it 'should raise error telling you so' do
+              expect {
+                response_definition.validate_content_type!(response)
+              }.to_not raise_error
             end
           end
+
+          context 'when content type does not match' do
+            let(:response_headers) { {'Content-Type' => "will_never_match" } }
+            it 'should raise error telling you so' do
+              expect {
+                response_definition.validate_content_type!(response)
+              }.to raise_error(/Bad Content-Type/)
+            end
+          end
+
+          context 'when content type is not set' do
+            let(:response_headers) { {} }
+            it 'should still raise an error' do
+              expect {
+                response_definition.validate_content_type!(response)
+              }.to raise_error(/Bad Content-Type/)
+            end
+          end
+        end
       end
 
       describe '#validate_parts!' do
-        let(:parts) { [] }
+        subject(:response) { BulkResponse.new(status: response_status, headers: response_headers) }
+
+        let(:part) { Praxis::MultipartPart.new('done', {'Status' => 200, 'Content-Type' => 'application/special'}) }
+
         before do
           response_definition.parts like: :ok, media_type: 'application/special'
-          allow(response).to receive(:parts).and_return(parts)
+          response.add_part(part)
         end
 
         it 'validates each part' do
-          p response_definition.parts
+          response_definition.parts
+          expect {
+            response_definition.validate_parts!(response)  
+          }.to_not raise_error
+        end
+
+        context 'with invalid part' do
+          let(:part) { Praxis::MultipartPart.new('done', {'Status' => 200, "Location" => "somewhere"}) }
+
+          it 'validates' do
+            expect {
+              response_definition.validate_parts!(response)
+            }.to raise_error(/Bad Content-Type/)
+          end
+
         end
 
       end
     end
 
   end
-#
-#  context 'multipart responses' do
-#        let(:part) { Praxis::MultipartPart.new('not so ok', {'Status' => 400, "Location" => "somewhere"}) }
-#
-#        context '#add_part' do
-#
-#          context 'without a name' do
-#            before do
-#              response.add_part(part)
-#            end
-#
-#            its(:parts) { should have(1).item }
-#
-#            it 'sets the Content-Type header' do
-#              expect(response.headers['Content-Type']).to match(/^multipart.*boundary=/i)
-#            end
-#
-#            it 'adds the part' do
-#              expect(response.parts.values.first).to be(part)
-#            end
-#          end
-#
-#          context 'with a name' do
-#            let(:part_name) { 'a-part' }
-#            before do
-#              response.add_part(part_name, part)
-#            end
-#
-#            it 'adds the part' do
-#              expect(response.parts[part_name]).to be(part)
-#            end
-#          end
-#
-#        end
-#
-#        context '#finish for a multipart response' do
-#
-#          before do
-#            response.add_part(part)
-#            response.body = 'a preamble'
-#            response.status = 500
-#          end
-#
-#          let!(:finished_response) { response.finish }
-#
-#          it 'returns status, headers, body' do
-#            expect(finished_response).to eq([response.status, response.headers, response.body])
-#          end
-#
-#          its(:status) { should eq(500) }
-#
-#          it 'sets a preamble in the body' do
-#            expect(response.body[0]).to eq('a preamble')
-#            expect(response.body[1]).to eq("\r\n")
-#          end
-#
-#          it 'sets the headers' do
-#            expect(response.headers['Content-Type']).to match(/multipart\/form-data/)
-#            expect(response.headers['Location']).to eq('/api/resources/123')
-#          end
-#
-#          it 'encodes the body properly' do
-#            parser = Praxis::MultipartParser.new(response.headers, response.body)
-#            preamble, parts = parser.parse
-#
-#            expect(preamble).to eq("a preamble")
-#            expect(parts).to have(1).item
-#
-#            _part_name, part_response = parts.first
-#            expect(part_response.headers['Status']).to eq(part.headers['Status'].to_s)
-#            expect(part_response.headers['Location']).to eq(part.headers['Location'])
-#            expect(part_response.body).to eq('not so ok')
-#          end
-#        end
-#
-#  end
-#
+
   context 'with invalid definitions' do
     it 'raises an error if status code is not part of the definition' do
       expect do
@@ -492,6 +434,8 @@ describe Praxis::ResponseDefinition do
   end
 
   context 'getting data from describe' do
+    subject(:response) { Praxis::Responses::Default.new(status: response_status, headers: response_headers) }
+
     it 'is a pending example'
   end
 end

f6a346b97551be7064abeabee19804de4de3d83a
Tony Spataro <tony@rightscale.com>
2015-01-26 14:40:30 -0800

    Add a description to praxis:console and make its behavior more consistent between pry and irb modes.
    Signed-off-by: Tony Spataro <tony@rightscale.com>

--- lib/praxis/tasks/console.rb
+++ lib/praxis/tasks/console.rb
@@ -1,11 +1,20 @@
 namespace :praxis do
-  task :console => :environment do
+  desc "Run interactive pry/irb console"
+  task :console do
     begin
+      # Use pry if available; require pry _before_ anything else to maximize
+      # debuggability.
       require 'pry'
-      pry
+      Rake::Task['environment'].invoke
+      Praxis::Application.instance.pry
     rescue LoadError
+      # Fall back on irb; use some special initialization magic to ensure that
+      # 'self' in the IRB session refers to Praxis::Application.instance.
       require 'irb'
-      IRB.start
-    end
+      Rake::Task['environment'].invoke
+      IRB.setup nil
+      IRB.conf[:MAIN_CONTEXT] = IRB::Irb.new.context
+      require 'irb/ext/multi-irb'
+      IRB.irb(nil, Praxis::Application.instance)
   end
 end

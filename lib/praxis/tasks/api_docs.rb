65d052271b5666b92a07c8b4d1a701773e0c1632
Josep M. Blanquer <blanquer@rightscale.com>
2015-03-12 16:10:03 -0700

    Simplified dependency name.
    Signed-off-by: Josep M. Blanquer <blanquer@rightscale.com>

--- lib/praxis/tasks/api_docs.rb
+++ lib/praxis/tasks/api_docs.rb
@@ -27,13 +27,13 @@ namespace :praxis do
     end
 
     desc "Run API Documentation Browser"
-    task :preview, [:port] => [:install, 'docs:generate']  do |t, args|      
+    task :preview, [:port] => [:install, :generate]  do |t, args|      
       doc_port = args[:port] || '9090'
       exec({'USER_DOCS_PATH' => File.join(Dir.pwd, 'docs'), 'DOC_PORT' => doc_port}, "#{path}/node_modules/.bin/grunt serve --gruntfile '#{path}/Gruntfile.js'")
     end
 
     desc "Build docs that can be shipped"
-    task :build => [:install, 'docs:generate'] do
+    task :build => [:install, :generate] do
       exec({'USER_DOCS_PATH' => File.join(Dir.pwd, 'docs')}, "#{path}/node_modules/.bin/grunt build --gruntfile '#{path}/Gruntfile.js'")
     end
 

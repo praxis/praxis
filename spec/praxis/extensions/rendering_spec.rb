7ce9aa37fa69718fcc71c21802232c9d878d251a
Josep M. Blanquer <blanquer@rightscale.com>
2015-10-30 10:45:06 -0700

    Added spec to rendering to capture CircularException
    Signed-off-by: Josep M. Blanquer <blanquer@rightscale.com>

--- spec/praxis/extensions/rendering_spec.rb
+++ spec/praxis/extensions/rendering_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Praxis::Extensions::Rendering do
 
   let(:test_class) do
-    Struct.new(:media_type, :expanded_fields, :response) do |klass|
+    Struct.new(:media_type, :expanded_fields, :response, :request) do |klass|
       include Praxis::Extensions::Rendering
     end
   end
@@ -11,9 +11,10 @@ describe Praxis::Extensions::Rendering do
   let(:media_type) { Person }
   let(:expanded_fields) { {id: true, name: true} }
   let(:response) { double('response', headers: {}) }
+  let(:request) { double('request') }
 
-  let(:object) { {id: 1, name: 'bob', href: '/people/bob'} }
-  subject(:instance) { test_class.new(media_type, expanded_fields, response) }
+  let(:object) { {id: '1', name: 'bob', href: '/people/bob'} }
+  subject(:instance) { test_class.new(media_type, expanded_fields, response, request) }
 
   context '#render' do
     subject(:output) { instance.render(object) }
@@ -23,18 +24,39 @@ describe Praxis::Extensions::Rendering do
   end
 
   context '#display' do
-    before do
-      expect(response).to receive(:body=).with({id: 1, name: 'bob'})
+    context 'without exception' do
+      before do
+        expect(response).to receive(:body=).with({id: 1, name: 'bob'})
+      end
+
+      subject!(:output) { instance.display(object) }
+
+      it 'returns the response' do
+        expect(output).to be(response)
+      end
+
+      it 'sets the Content-Type header' do
+        expect(response.headers['Content-Type']).to eq 'application/vnd.acme.person'
+      end
     end
 
-    subject!(:output) { instance.display(object) }
-
-    it 'returns the response' do
-      expect(output).to be(response)
-    end
-
-    it 'sets the Content-Type header' do
-      expect(response.headers['Content-Type']).to eq 'application/vnd.acme.person'
+    context 'with a rendering exception' do
+      let(:handler_params) do
+        {
+          summary: "Circular Rendering Error when rendering response. " +
+                 "Please especify a view to narrow the dependent fields, or narrow your field set.",
+          exception: circular_exception,
+          request: request,
+          stage: :action,
+          errors: nil
+        }
+      end
+      let(:circular_exception){ Praxis::Renderer::CircularRenderingError.new(object, "ctx") }
+      it 'catches a circular rendering exception' do
+        expect(instance).to receive(:render).and_raise(circular_exception)
+        expect(Praxis::Application.instance.validation_handler).to receive(:handle!).with(handler_params)
+        instance.display(object)
+      end
     end
   end
 

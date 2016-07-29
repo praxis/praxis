326fdb00c6e31905145e7d2fdc878b5c07aeb35c
Josep M. Blanquer <blanquer@gmail.com>
2016-07-28 17:02:28 -0700

    Helper for Praxis as an intercepting Middleware (#313)
    * Build a helper to mount a Praxis application as an intercepting middleware which would handle requests, and only forward requests that cannot be properly handled by it (respecting X-Cascade).
    * use the proper splatting args for `setup`
    
    Signed-off-by: Josep M. Blanquer <blanquer@gmail.com>

---
+++ lib/praxis/middleware_app.rb
@@ -0,0 +1,30 @@
+module Praxis
+  class MiddlewareApp
+
+    attr_reader :target
+
+    # Initialize the application instance with the desired args, and return the wrapping class.
+    def self.for( **args )
+      Praxis::Application.instance.setup(**args)
+      self
+     end
+
+    def initialize( inner )
+      @target = inner
+    end
+
+    def call(env)
+      result = Praxis::Application.instance.call(env)
+
+      unless ( [404,405].include?(result[0].to_i) && result[1]['X-Cascade'] == 'pass' )
+        # Respect X-Cascade header if it doesn't specify 'pass'
+        result
+      else
+        last_body = result[2]
+        last_body.close if last_body.respond_to? :close
+        target.call(env)
+      end
+    end
+
+  end
+end

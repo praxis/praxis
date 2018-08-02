02fe792cd9379c0551bff5f715b448ffda3fd420
Josep M. Blanquer <blanquer@gmail.com>
2018-08-01 17:34:43 -0700

    wip - This makes one app that uses the MWare App seemingly run (albeit the hacks for now)

--- lib/praxis/middleware_app.rb
+++ lib/praxis/middleware_app.rb
@@ -14,16 +14,27 @@ module Praxis
         def self.args
           @args
         end
+        
       end
      end
 
     def initialize( inner )
       @target = inner
-      @app_instance = false
+      @app_instance = Praxis::Application.new
+      $josep_hack_mware = self
     end
 
+    def setup
+      # This function is to allow the app initialization to set things up at the right time for now...
+      # We need to have a better way to "reach" into the application instances from the middlewares registered 
+      # in the bigger app...and call their setup... (this one...)
+      puts "SETTING UP!!!#{self.class.args}" 
+      @app_instance.setup(**self.class.args)
+    end
+    
     def call(env)
-      @app_instance ||= Praxis::Application.new.setup(**self.class.args)
+      # NOTE: Need to make sure somebody has properly called the setup above before this is called
+      #@app_instance ||= Praxis::Application.new.setup(**self.class.args) #I Think that's not right at all...
       result = @app_instance.call(env)
 
       unless ( [404,405].include?(result[0].to_i) && result[1]['X-Cascade'] == 'pass' )

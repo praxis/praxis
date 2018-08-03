08de0afe8c8cffc32100365806050f268425164c
Josep M. Blanquer <blanquer@gmail.com>
2018-08-02 17:51:23 -0700

    focus on fixing the doc generation as well…

--- lib/praxis/middleware_app.rb
+++ lib/praxis/middleware_app.rb
@@ -2,40 +2,42 @@ module Praxis
   class MiddlewareApp
 
     attr_reader :target
-
     # Initialize the application instance with the desired args, and return the wrapping class.
     def self.for( **args )
       Class.new(self) do
-
+        class << self
+          attr_accessor :app_instance
+          attr_reader :app_name
+        end
+        @app_name = args.delete(:name)
         @args = args
+        @app_instance = nil
+        
         def self.name
           'MiddlewareApp'
         end
         def self.args
           @args
         end
-        
+        def self.setup
+          puts "SETTING UP CLASS!!!#{args}" 
+          app_instance.setup(**args)
+        end
       end
-     end
-
-    def initialize( inner )
-      @target = inner
-      @app_instance = Praxis::Application.new
-      $josep_hack_mware = self
     end
 
-    def setup
-      # This function is to allow the app initialization to set things up at the right time for now...
-      # We need to have a better way to "reach" into the application instances from the middlewares registered 
-      # in the bigger app...and call their setup... (this one...)
-      puts "SETTING UP!!!#{self.class.args}" 
-      @app_instance.setup(**self.class.args)
+    def initialize( inner )
+      puts "SELF: #{self}"
+      @target = inner
+      self.class.app_instance = Praxis::Application.new(name: self.class.app_name)
+      
+      #$josep_hack_mware = self
     end
     
     def call(env)
       # NOTE: Need to make sure somebody has properly called the setup above before this is called
       #@app_instance ||= Praxis::Application.new.setup(**self.class.args) #I Think that's not right at all...
-      result = @app_instance.call(env)
+      result = self.class.app_instance.call(env)
 
       unless ( [404,405].include?(result[0].to_i) && result[1]['X-Cascade'] == 'pass' )
         # Respect X-Cascade header if it doesn't specify 'pass'

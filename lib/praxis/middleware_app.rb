c8e0d9bf2c5b981310e3dbfd3546b220e507e225
Josep M. Blanquer <blanquer@gmail.com>
2019-10-04 17:23:34 -0700

    expose the MiddlewareApp class setup method to allow initialization using the mware parameters directly

--- lib/praxis/middleware_app.rb
+++ lib/praxis/middleware_app.rb
@@ -6,13 +6,20 @@ module Praxis
     # Initialize the application instance with the desired args, and return the wrapping class.
     def self.for( **args )
       Class.new(self) do
-
         @args = args
+        @setup_done = false
         def self.name
           'MiddlewareApp'
         end
         def self.args
           @args
+        end
+        def self.setup_done
+          @setup_done
+        end
+        def self.setup
+          @setup_done = true
+          Praxis::Application.instance.setup(**@args)
         end
       end
      end
@@ -23,10 +30,8 @@ module Praxis
     end
 
     def call(env)
-      unless @setup_done
-        Praxis::Application.instance.setup(**self.class.args)
-        @setup_done = true
-      end
+      self.class.setup unless self.class.setup_done
+      
       result = Praxis::Application.instance.call(env)
 
       unless ( [404,405].include?(result[0].to_i) && result[1]['X-Cascade'] == 'pass' )

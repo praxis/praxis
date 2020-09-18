3bfc4be10a4b506303461164cc43c91abe0f1bac
Josep M. Blanquer <blanquer@gmail.com>
2020-09-18 12:08:45 -0700

    separate AR and Sequel , and make it a parameter for the handle method

--- lib/praxis/extensions/pagination/pagination_handler.rb
+++ lib/praxis/extensions/pagination/pagination_handler.rb
@@ -50,72 +50,20 @@ module Praxis
         end
 
         def self.where_lt(query, attr, value)
-          # TODO: common for AR/Sequel? Seems we could use Arel and more-specific Sequel things
-          query.where(query.table[attr].lt(value))
-          #query.where("#{attr} < ?", value)
+          raise "implement in derived class"
         end
         
         def self.where_gt(query, attr, value)
-          query.where(query.table[attr].gt(value))
-          # query.where("#{attr} > ?", value)
+          raise "implement in derived class"
         end
 
         def self.offset(query, offset)
-          # TODO: common for AR/Sequel
-          query.offset(offset)
+          raise "implement in derived class"
         end
 
         def self.limit(query, limit)
-          # TODO: common for AR/Sequel
-          query.limit(limit)
+          raise "implement in derived class"
         end
-
-        def self.order(query, order)
-          # TODO: hardcoding
-          activerecord_order(query,order)
-        end
-        
-        def self.count(query)
-          # TODO: hardcoding
-          activerecord_count(query)
-        end
-
-        ####################################
-
-        def self.activerecord_count(query)
-          query.count(:all)
-        end
-
-        def self.sequel_count(query)
-          query.count
-        end
-
-        def self.sequel_order(query, order)
-          return query unless order
-          order_clause = order.map do |spec_hash|
-            direction, name = spec_hash.first
-            case direction.to_sym
-            when :desc
-              Sequel.desc(name.to_sym)
-            else
-              Sequel.asc(name.to_sym)
-            end
-          end
-          query = query.order(*order_clause)
-          query
-        end
-
-        def self.activerecord_order(query, order)
-          return query unless order
-          query = query.reorder('')
-          
-          order.each do |spec_hash|
-            direction, name = spec_hash.first
-            query = query.order(name => direction)
-          end
-          query
-        end
-        
       end
     end
   end

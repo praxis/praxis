5be2239697b29e4e1a2f111b196d2be9da160688
Josep M. Blanquer <blanquer@gmail.com>
2020-10-12 16:23:24 +0200

    wip - cleanup and trying to choose better aliases (full path)

--- lib/praxis/extensions/attribute_filtering/active_record_filter_query_builder.rb
+++ lib/praxis/extensions/attribute_filtering/active_record_filter_query_builder.rb
@@ -13,22 +13,19 @@ module Praxis
 
         def build_clause(filters)
           filters.each do |item|
-            attr = item[:name]
             spec = item[:specs]
-            column_name = attr_to_column[attr]
+            column_name = attr_to_column[item[:name]]
             raise "Filtering by #{attr} not allowed (no mapping found)" unless column_name
-            if column_name.is_a?(Proc)
-              bindings = column_name.call(spec)
-              # A hash of bindings, consisting of a key with column name and a value to the query value
-              # TODO: just get the bindings here...and use a single 2 liner at the end of the build clause ... since it's repeated
-              bindings.each do|col,val|
-                assoc_or_field, *rest = col.to_s.split('.')
-                expand_binding(first_segment: assoc_or_field, rest: rest, op: spec[:op], value: val, use_this_name_for_clause: self.table)
+            bindings = \
+              if column_name.is_a?(Proc)
+                column_name.call(spec)
+              else
+                {column_name => spec[:value] } # For non-procs there's only 1 filter and 1 value
               end
-            else
-              assoc_or_field, *rest = column_name.to_s.split('.')
-              puts "FILTERING FOR: #{assoc_or_field}"
-              expand_binding(first_segment: assoc_or_field, rest: rest, **spec, use_this_name_for_clause: self.table)
+
+            bindings.each do|filter_segments,filter_value|
+              first_segment, *rest = filter_segments.to_s.split('.')
+              expand_binding(first_segment: first_segment, rest: rest, op: spec[:op], value: filter_value, use_this_name_for_clause: self.table)
             end
           end
           query
@@ -42,7 +39,7 @@ module Praxis
 
         def do_join_reflection( query, reflection, source_alias, table_alias )
           c = query.connection
-          case reflection
+          ref = case reflection
           when ActiveRecord::Reflection::BelongsToReflection
             join_clause = "INNER JOIN %s as %s ON %s.%s = %s.%s " % \
                   [
@@ -55,8 +52,8 @@ module Praxis
                   ]
             query.joins(join_clause)
           when ActiveRecord::Reflection::HasManyReflection
-      #        join_clause = "INNER JOIN #{reflection.klass.table_name} as #{table_alias} ON"  + \
-      #              " \"#{source_alias}\".\"id\" = \"#{table_alias}\".\"#{reflection.foreign_key}\" "
+            # join_clause = "INNER JOIN #{reflection.klass.table_name} as #{table_alias} ON"  + \
+            #       " \"#{source_alias}\".\"id\" = \"#{table_alias}\".\"#{reflection.foreign_key}\" "
             join_clause = "INNER JOIN %s as %s ON %s.%s = %s.%s " % \
                   [c.quote_table_name(reflection.klass.table_name),
                     c.quote_table_name(table_alias),
@@ -66,9 +63,9 @@ module Praxis
                     c.quote_column_name(reflection.foreign_key)
                   ]
 
-            # TODO: This is to support polymorphic things I think...redo
+            # Polymorphic type, add the appropriate condition to restrict the result to the right type
             if reflection.type # && reflection.options[:as]....
-      #          addition = " AND \"#{table_alias}\".\"#{reflection.type}\" = \'#{reflection.active_record.class_name}\'"
+              #addition = " AND \"#{table_alias}\".\"#{reflection.type}\" = \'#{reflection.active_record.class_name}\'"
               addition = " AND %s.%s = %s" % \
               [ c.quote_table_name(table_alias),
                 c.quote_table_name(reflection.type),
@@ -77,10 +74,12 @@ module Praxis
               join_clause += addition
             end
             query.joins(join_clause)
+
           when ActiveRecord::Reflection::ThroughReflection
             talias = reflection.through_reflection.table_name
             salias = source_alias
 
+            puts "FIRST"
             query = do_join_reflection(query, reflection.through_reflection, salias, talias)
             #puts "TODO: choose different alias ?????????"
             salias = talias
@@ -88,22 +87,23 @@ module Praxis
             through_model = reflection.through_reflection.klass
             through_assoc = reflection.name
             final_reflection = reflection.source_reflection
-
+            puts "LAST"
             do_join_reflection(query, final_reflection, salias, table_alias)
           else
             raise "Joins for this association type are currently UNSUPPORTED: #{reflection.inspect}"
           end
+          puts ">>>#{ref.to_sql}"
+          ref
         end
 
         def expand_binding(first_segment:, rest: , op:, value:, use_this_name_for_clause:)
-          unless rest.empty?
+          if rest.empty?
+            expanded_column_name = "#{use_this_name_for_clause}.#{first_segment}"
+            add_clause(column_name: expanded_column_name, op: op, value: value)
+          else # Join and continue with more segments
             @query = do_join(query, first_segment, use_this_name_for_clause, first_segment)
             new_first_segment, *new_rest = rest
             expand_binding(first_segment: new_first_segment, rest: new_rest, op: op, value: value, use_this_name_for_clause: first_segment)
-          else
-            new_first_segment = "#{use_this_name_for_clause}.#{first_segment}"
-            puts "EXPAND FOR: #{first_segment} [join alias: #{use_this_name_for_clause}]"
-            add_clause(column_name: new_first_segment, op: op, value: value)
           end
         end
 

b8c7a74520825a1629448088b0bde1a00d0024c1
Josep M. Blanquer <blanquer@gmail.com>
2021-02-23 00:22:49 -0800

    Rebuild api filters for complex syntax (#347)
    * Build a completely new parser for Filters which supports AND/ORs and parenthesis (ANDs have higher precdence)
    Rebuilt ActiveRecordFilterQuery to use the new parser and appropriately build the queries.
    
    * some formatting and dead code removal

--- spec/praxis/extensions/attribute_filtering/filter_tree_node_spec.rb
+++ spec/praxis/extensions/attribute_filtering/filter_tree_node_spec.rb
@@ -4,14 +4,15 @@ require 'praxis/extensions/attribute_filtering'
 
 describe Praxis::Extensions::AttributeFiltering::FilterTreeNode do
 
+  let(:dummy_object) { double("Fake NodeObject")}
   let(:filters) do
     [
-      {name: 'one', specs: { op: '>', value: 1}},
-      {name: 'one', specs: { op: '<', value: 10}},
-      {name: 'rel1.a1', specs: { op: '=', value: 1}},
-      {name: 'rel1.a2', specs: { op: '=', value: 2}},
-      {name: 'rel1.rel2.b1', specs: { op: '=', value: 11}},
-      {name: 'rel1.rel2.b2', specs: { op: '=', value: 12}}
+      {name: 'one', op: '>', value: 1, node_object: dummy_object},
+      {name: 'one', op: '<', value: 10},
+      {name: 'rel1.a1', op: '=', value: 1},
+      {name: 'rel1.a2', op: '=', value: 2},
+      {name: 'rel1.rel2.b1', op: '=', value: 11},
+      {name: 'rel1.rel2.b2', op: '=', value: 12, node_object: dummy_object}
     ]
   end
   context 'initialization' do
@@ -19,20 +20,38 @@ describe Praxis::Extensions::AttributeFiltering::FilterTreeNode do
     it 'holds the top conditions and the child in a TreeNode' do
       expect(subject.path).to eq([])
       expect(subject.conditions.size).to eq(2)
+      expect(subject.conditions.map{|i| i.slice(:name,:op,:value)}).to eq([
+        {name: 'one', op: '>', value: 1},
+        {name: 'one', op: '<', value: 10},
+      ])
       expect(subject.children.keys).to eq(['rel1'])
       expect(subject.children['rel1']).to be_kind_of(described_class)
+    end
+
+    it 'passes on any node_object value at any level' do
+      expect(subject.conditions.first[:node_object]).to be(dummy_object)
+      expect(subject.conditions[1]).to_not have_key(:node_object)
+      expect(subject.children['rel1'].children['rel2'].conditions[1][:node_object]).to be(dummy_object)
     end
 
     it 'recursively holds the conditions and the children of their children in a TreeNode' do
       rel1 = subject.children['rel1']
       expect(rel1.path).to eq(['rel1'])
       expect(rel1.conditions.size).to eq(2)
+      expect(rel1.conditions.map{|i| i.slice(:name,:op,:value)}).to eq([
+        {name: 'a1', op: '=', value: 1},
+        {name: 'a2', op: '=', value: 2},
+      ])
       expect(rel1.children.keys).to eq(['rel2'])
       expect(rel1.children['rel2']).to be_kind_of(described_class)
 
       rel1rel2 = rel1.children['rel2']
       expect(rel1rel2.path).to eq(['rel1','rel2'])
       expect(rel1rel2.conditions.size).to eq(2)
+      expect(rel1rel2.conditions.map{|i| i.slice(:name,:op,:value)}).to eq([
+        {name: 'b1', op: '=', value: 11},
+        {name: 'b2', op: '=', value: 12}  
+      ])      
       expect(rel1rel2.children.keys).to be_empty
     end
   end

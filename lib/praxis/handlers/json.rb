f4224cb43bcc2146d6fc0842b753178dde8263ef
Josep M. Blanquer <blanquer@C02F20XQMD6W.local>
2022-10-21 18:55:12 +0200

    switch to using Oj for JSON loading/dumping (using its builtin JSON mimicing)

--- lib/praxis/handlers/json.rb
+++ lib/praxis/handlers/json.rb
@@ -7,11 +7,17 @@ module Praxis
       #
       # @raise [Praxis::Exceptions::InvalidConfiguration] if the handler is unsupported
       def initialize
-        require 'json'
+        require 'oj'
+        begin
+          require 'json'
+        rescue LoadError # rubocop:disable Lint/SuppressedException
+        end
+        # Enable mimicing needs to be done after loading the JSON gem (if there)
+        ::Oj.mimic_JSON
       rescue LoadError
         # Should never happen since JSON is a default gem; might as well be cautious!
         raise Praxis::Exceptions::InvalidConfiguration,
-              'JSON handler depends on json ~> 1.0; please add it to your Gemfile'
+              'JSON handler depends on oj ~> 3; please add it to your Gemfile'
       end
 
       # Parse a JSON document into structured data.
@@ -22,7 +28,7 @@ module Praxis
         # Try to be nice and accept an empty string as an empty payload (seems nice to do for dumb http clients)
         return nil if document.nil? || document == ''
 
-        ::JSON.parse(document)
+        ::Oj.load(document)
       end
 
       # Generate a pretty-printed JSON document from structured data.
@@ -30,7 +36,7 @@ module Praxis
       # @param [Hash,Array] structured_data
       # @return [String]
       def generate(structured_data)
-        ::JSON.pretty_generate(structured_data)
+        ::Oj.dump(structured_data, indent: 2)
       end
     end
   end

1e76abdb88260942101991d171439b1ccad64729
Josep M. Blanquer <blanquer@C02F20XQMD6W.local>
2023-02-03 17:28:11 +0100

    example inner group object with forwarder calls

--- spec/spec_app/app/resources/book.rb
+++ spec/spec_app/app/resources/book.rb
@@ -1,6 +1,32 @@
 require_relative 'base'
 
 module Resources
+  class Forwarderer
+    extend Forwardable
+    attr_accessor :target
+
+    def self.for(names)
+      Class.new(self) do
+        
+        names.map(&:to_sym).each do |spec|
+          if spec.is_a? Symbol
+            def_delegator :@target, spec
+          else
+            name = spec.keys.first
+            def_delegator :@target, name, spec[name]
+          end
+        end
+      end  
+    end
+    def initialize(target)
+      @target = target
+    end
+
+    def foo
+      target.name
+    end
+  end
+
   class Book < Resources::Base
     model ::ActiveBook
 
@@ -20,7 +46,8 @@ module Resources
 
     property :grouped, dependencies: [:simple_name, :category_uuid] # TODO: Dependency resolution should have kicked in when asking for 'grouped' without any inner ones...
     def grouped
-      self
+      @_grouped_fwd ||= Forwarderer.for([:id, :name])
+      @_grouped_fwd.new(self) # This shouldn't be a 'new' ... but a class new...
     end
   end
 end

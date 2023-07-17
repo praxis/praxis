7b7b1ea02f7bc2d8367b86b4402265a8c4db29f6
Tony Spataro <anthony.spataro@appfolio.com>
2023-07-17 10:49:06 -0700

    Deal with ActiveRecord 7 optimizations (#397)
    * Ignore appraisal lockfiles (just like we ignore the root lockfile)
    
    * Deal with AR7 optimizations
    
    * Clarify specs (peer review feedback)

--- lib/praxis/extensions/field_selection/active_record_query_selector.rb
+++ lib/praxis/extensions/field_selection/active_record_query_selector.rb
@@ -29,9 +29,27 @@ module Praxis
           # We're gonna always require the PK of the model, as it is a special case for AR, and the app itself
           # might assume it is always there and not be surprised by the fact that if it isn't, it won't blow up
           # in the same way as any other attribute not being loaded...i.e., ActiveModel::MissingAttributeError: missing attribute: xyz
-          select_fields = selector_node.select + [selector_node.resource.model.primary_key.to_sym]
+          select_fields =
+            _hoist_select(
+              selector_node: selector_node,
+              fields_closure:
+                Set.new([selector_node.resource.model.primary_key.to_sym]),
+            ).to_a
           select_fields.empty? ? query : query.select(*select_fields)
         end
+
+        def explain_query(query, eager_hash)
+          @logger.debug(
+            "Query plan for ...#{selector.resource.model} with selectors: #{JSON.generate(selector.dump)}",
+          )
+          @logger.debug(
+            " ActiveRecord query: #{selector.resource.model}.includes(#{eager_hash})",
+          )
+          query.explain
+          @logger.debug('Query plan end')
+        end
+
+        private
 
         def _eager(selector_node)
           selector_node.tracks.transform_values do |track_node|
@@ -39,11 +57,34 @@ module Praxis
           end
         end
 
-        def explain_query(query, eager_hash)
-          @logger.debug("Query plan for ...#{selector.resource.model} with selectors: #{JSON.generate(selector.dump)}")
-          @logger.debug(" ActiveRecord query: #{selector.resource.model}.includes(#{eager_hash})")
-          query.explain
-          @logger.debug('Query plan end')
+        # This deals with a performance optimization introduced in ActiveRecord 7
+        # When preloading associations, they now reuse model instances that have already
+        # been loaded as part of the same chain of queries.
+        #
+        # If the root uses a `select` constraint, then some attributes may not be loaded
+        # if the record is reused elsewhere in the resulting graph of models. In former
+        # versions of ActiveRecord, a `SELECT *` would have been used to instantiate these
+        # nested versions of the model and so all attributes would have been loaded.
+        #
+        # To account for this discrepancy, we hoist all transitive columns of the root
+        # model up to the root of the query tree, preserving the optimization benefit
+        # of a narrow field selector while ensuring that all requested fields are
+        # available at every node in the model graph.
+        #
+        # @return [Set<Symbol]
+        def _hoist_select(root_node: nil, selector_node:, fields_closure:)
+          root_node ||= selector_node
+          if root_node.resource == selector_node.resource
+            fields_closure.merge(selector_node.select)
+          end
+          selector_node.tracks.values.each do |track_node|
+            _hoist_select(
+              root_node: root_node,
+              selector_node: track_node,
+              fields_closure: fields_closure,
+            )
+          end
+          fields_closure
         end
       end
     end
